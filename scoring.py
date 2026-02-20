17a7315bdf2b26c02fd225f3f5032b12f22375ff
bigben-dev777 <albertojohn20041103@gmail.com>
2026-02-20 07:02:48 -0500

    change the all

--- scoring.py
+++ scoring.py
@@ -17,13 +17,14 @@ def reward(y_pred: np.ndarray, y_true: np.ndarray) -> tuple[float, dict]:
     """
     preds = np.round(y_pred).astype(int)
     cm = confusion_matrix(y_true, preds, labels=[0, 1])
-    tn, fp, fn, tp = cm.ravel()
+    _, fp, _, tp = cm.ravel()
     f1 = f1_score(y_true, preds) if (tp + fp) > 0 else 0.0
     ap_score = average_precision_score(y_true, y_pred) if y_pred.size else 0.0
 
     res = {
         "fp_score": 1 - fp / max(len(y_pred), 1),
-        "f1_score": f1
+        "f1_score": f1,
+        "approve": ap_score
     }
     rew = sum(res.values()) / len(res)
     return rew, res
